d706c49063e42571bb8b42c7d896160cae774aee
majomarquez <majomarquez@gmail.com>
2019-05-14 18:16:06 -0400

    className en App.js , background Home, css home ( background, imgsize , font)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
+import './App.css'
 
 import logoBurger from './images/logo.png';
 import breakfastBurger from './images/desayuno.jpg';
 import lunchBurger from './images/almuerzo.jpg';
 
+
 class App extends React.Component{
     render(){
         
         return (
-            <div className="Home" >
-                <div className="Home_header">
-                    <img src={logoBurger} alt="Logo"/>
-                </div>
-
-                <div className="Home_customer">
+            
+            <div className = "home" >
+                <div className="home_header">
+                    <img className="home_logo" src={logoBurger} alt="Logo"/>
                     <h1>{this.props.customer}</h1>
                 </div>
 
-                <div className="Home_breakfast">
-                    <img src= {breakfastBurger} alt="img breakfast"/>
+                <div className="home_breakfast">
+                    <img className="home_img_breakfast"src= {breakfastBurger} alt="img breakfast"/>
                     <h1>{this.props.breakfast}</h1>
                 </div>  
 
-                <div className="Home_lunch">
-                    <img src={lunchBurger} alt="img lunch"/>
+                <div className="home_lunch">
+                    <img className="home_img_lunch" src={lunchBurger} alt="img lunch"/>
                     <h1>{this.props.lunch}</h1>
                 </div>  
             </div> 
