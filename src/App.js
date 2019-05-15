4f62cb0395da16731ae46d0ae618ce58c8a4f87a
majomarquez <majomarquez@gmail.com>
2019-05-15 16:25:02 -0400

    grid bootstrap, backgraund home

--- src/App.js
+++ src/App.js
@@ -10,23 +10,37 @@ class App extends React.Component{
     render(){
         
         return (
-            
             <div className = "home" >
-                <div className="home_header">
-                    <img className="home_logo" src={logoBurger} alt="Logo"/>
-                    <h1>{this.props.customer}</h1>
-                </div>
-
-                <div className="home_breakfast">
-                    <img className="home_img_breakfast"src= {breakfastBurger} alt="img breakfast"/>
-                    <h1>{this.props.breakfast}</h1>
-                </div>  
-
-                <div className="home_lunch">
-                    <img className="home_img_lunch" src={lunchBurger} alt="img lunch"/>
-                    <h1>{this.props.lunch}</h1>
-                </div>  
-            </div> 
+                <div className="container-fluid">
+                    
+                    <div className="home_header">
+                        <div className= "row">
+                            <div className ="col" >
+                                <img className="home_logo" src={logoBurger} alt="Logo"/>
+                            </div>
+                            <div className ="col" >
+                                <h1 className="name">{this.props.customer}</h1>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div className= "row">
+                        <div className ="col" >
+                            <div className="home_breakfast">
+                                <img className="home_img_breakfast"src= {breakfastBurger} alt="img breakfast"/>
+                                <h1>{this.props.breakfast}</h1>
+                            </div> 
+                        </div>
+                         
+                        <div className ="col" >
+                            <div className="home_lunch">
+                                <img className="home_img_lunch" src={lunchBurger} alt="img lunch"/>
+                                <h1>{this.props.lunch}</h1>
+                            </div>
+                        </div> 
+                    </div> 
+                </div> 
+            </div>
         )}
 }
 
